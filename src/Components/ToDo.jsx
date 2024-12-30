1fd3a2d8234a8dba247f77b4f346b07a03fca461
Saikat Singha <saikatsingha531@gmail.com>
2024-12-31 01:46:35 +0530

    second commit

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./ToDo.css"
+import { MdDeleteSweep } from "react-icons/md";
+import { IoMdDoneAll } from "react-icons/io";
+
 const ToDo = () => {
   return (
     <>
-    <div className="container">
+    <div className="mainContainer">
+        <div className="container">
+        <h1>Add Your To-Do</h1>
+        <div className="addBox">
+            <input type="text" />
+            <button id='add'>Add</button>
+        </div>
 
+        <div className="todo">
+            <button className='done'><IoMdDoneAll /></button>
+            <div className='list'>Firat to do</div>
+            <button className='delete'><MdDeleteSweep /></button>
+            
+        </div>
+        </div>
     </div>
     </>
   )
