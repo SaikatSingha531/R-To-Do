bef409fb6b6b20b96f36e20fddab535efa008cf2
Saikat Singha <saikatsingha531@gmail.com>
2025-01-01 23:53:33 +0530

    creating todo list

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./ToDo.css"
 import { MdDeleteSweep } from "react-icons/md";
 import { IoMdDoneAll } from "react-icons/io";
 
 const ToDo = () => {
+
+
+  const [inputVal, setinputVal] = useState("");   // this is for taking the input value 
+
+  const textVal = (i)=> {          //for show the written value in input 
+    setinputVal(i.target.value)
+  }
+
+  const formSubmit= (i)=>{         // for stop form defoult behavior
+
+     if (!inputVal) return;
+    i.preventDefault();
+  }
+
+
+  const [todoVal, settodoVal] = useState([]);
+
+
+    const [IsDone, setIsDone] = useState(false);
+
+    // const doneClick = ()=>{
+    //     setIsDone(!IsDone)
+    // }
+
   return (
     <>
+    <form onSubmit={formSubmit}>
     <div className="mainContainer">
         <div className="container">
         <h1>Add Your To-Do</h1>
         <div className="addBox">
-            <input type="text" />
+            <input type="text" value={inputVal} onChange={textVal} placeholder='Enter Your Task..' />
             <button id='add'>Add</button>
         </div>
 
         <div className="todo">
-            <button className='done'><IoMdDoneAll /></button>
-            <div className='list'>Firat to do</div>
+           <div className="todos">
+           <button className='done'><IoMdDoneAll /></button>
+            <div className='list' >Firat to do</div>
             <button className='delete'><MdDeleteSweep /></button>
+           </div>
+           {/* <div className="todos">
+           <button className='done' onClick={doneClick}><IoMdDoneAll /></button>
+            <div className='list' style={{textDecoration:IsDone ? "line-through" : "none" , color: IsDone ? "gray" : "black", transition: "color 0.3s ease",}}>Second to do</div>
+            <button className='delete'><MdDeleteSweep /></button>
+           </div> */}
             
         </div>
         </div>
     </div>
+    </form>
     </>
   )
 }
